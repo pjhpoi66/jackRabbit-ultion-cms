400c1e3d47528f135e06285bac67cfe2227e2248
pjhpoi66 <duncan21sas@naver.com>
2022-02-04 15:15:06 +0900

    test commit

--- src/main/java/com/ultion/cms/JackRabbitUltionCmsApplication.java
+++ src/main/java/com/ultion/cms/JackRabbitUltionCmsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("${spring.config.path}")
+@ImportResource("classpath:application-context.xml")
 public class JackRabbitUltionCmsApplication {
 
     public static void main(String[] args) {
