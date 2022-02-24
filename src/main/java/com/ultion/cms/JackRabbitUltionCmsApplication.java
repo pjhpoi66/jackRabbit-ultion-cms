e59b452127fab6ef0523fa78c535a3ea9c6440d1
pjhpoi66 <duncan21sas@naver.com>
2022-02-24 17:15:36 +0900

    2022.02.24 - 박중호 : JackRabbitUltionCmsApplication 수정

--- src/main/java/com/ultion/cms/JackRabbitUltionCmsApplication.java
+++ src/main/java/com/ultion/cms/JackRabbitUltionCmsApplication.java
@@ -1,15 +1,26 @@
 package com.ultion.cms;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 @ImportResource("classpath:application-context.xml")
+@Slf4j
 public class JackRabbitUltionCmsApplication {
+
+    static String buildDesc = "2022.02.04 - 박중호 : Ultion CMS project init";
 
     public static void main(String[] args) {
         SpringApplication.run(JackRabbitUltionCmsApplication.class, args);
+        log.info("=========================================================");
+        log.info("Build Instance : {}", JackRabbitUltionCmsApplication.class.getName());
+        log.info("Build Time : {}", LocalDateTime.now());
+        log.info("Build Description : {}", buildDesc);
+        log.info("=========================================================");
     }
 
 }
